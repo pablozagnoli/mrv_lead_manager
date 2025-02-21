4c436f814dd9a53c9876d2c6634e1e1bfd9d083c
pablo zagnoli <pablozagnoli@outlook.com>
2025-02-21 09:01:36 -0300

    update lead

---
+++ mrv_lead_manager_front/src/app/DTOs/lead.ts
@@ -0,0 +1,3 @@
+export class LeadDTO {
+  id: number | undefined;
+}
--- mrv_lead_manager_front/src/app/components/home/home.component.ts
+++ mrv_lead_manager_front/src/app/components/home/home.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
-import { LeadsComponent } from "../leads/leads.component";
+import { LeadsComponent } from '../leads/leads.component';
+import { HomeService } from './service/home.service';
+import { LeadDTO } from '../../DTOs/lead';
 
 @Component({
   selector: 'app-home',
   imports: [LeadsComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
+  constructor(private service: HomeService) {}
 
-  leadsInvited(){
-    alert("Invited")
+  leadsInvited(lead: LeadDTO) {
+    this.service.updateLead(lead).subscribe({
+      next: (result) => {},
+      error: (error) => {
+        alert('ERRO');
+      },
+    });
   }
-  leadsAccepted(){
-    alert("Accepted")
+
+  leadsAccepted(lead: LeadDTO) {
+    this.service.updateLead(lead).subscribe({
+      next: (result) => {},
+      error: (error) => {
+        alert('ERRO');
+      },
+    });
   }
 }
